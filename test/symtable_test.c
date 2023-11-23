ba1db4ee4e322a986a9cb0ab6895b7b847001427
izem <ipsy2k17@gmail.com>
2023-11-23 16:49:23 +0100

    symtable tests changes

--- test/symtable_test.c
+++ test/symtable_test.c
@@ -3,6 +3,8 @@
 /*  implementace: Maksim Dubrovin (xdubro01)        */
 /* **************************************************/
 
+#include <stdlib.h>
+#include "../data_structures/str.h"
 #include "unity.h"
 #include "../data_structures/symtable.h"
 
@@ -21,13 +23,23 @@ void test_insert_and_get_function(void) {
     char* key2 = "test_key2";
     char* key3 = "test_key3";
 
-    symtable_insert_function(table, key1, NONE_TYPE, 1);
-    symtable_insert_function(table, key2, INTEGER, 2);
-    symtable_insert_function(table, key3, STRING, 3);
+    char* val1 = copy_string("val1");
+    char* val2 = copy_string("val2");
+    char* val3 = copy_string("val3");
+
+    char *arr1[256] = {val1};
+    char *arr2[256] = {val1, val2};
+    char *arr3[256] = {val1, val2, val3};
+
+
+    symtable_insert_function(table, key1, NONE_TYPE, 1, arr1);
+    symtable_insert_function(table, key2, INTEGER, 2, arr2);
+    symtable_insert_function(table, key3, STRING, 3, arr3);
 
     funcInfo *info = symtable_get_pair(table, key1);
     TEST_ASSERT_TRUE(info->numOfParams == 1);
     TEST_ASSERT_TRUE(info->returnType == NONE_TYPE);
+    TEST_ASSERT_TRUE(compare_strings(info->paramNames[0], "val1") == 1);
 
     info = symtable_get_pair(table, key2);
     TEST_ASSERT_TRUE(info->numOfParams == 2);
@@ -36,7 +48,66 @@ void test_insert_and_get_function(void) {
     info = symtable_get_pair(table, key3);
     TEST_ASSERT_TRUE(info->numOfParams == 3);
     TEST_ASSERT_TRUE(info->returnType == STRING);
+    TEST_ASSERT_TRUE(compare_strings(info->paramNames[2], "val3") == 1);
     symtable_delete_table(table);
+    free(val1);
+    free(val2);
+    free(val3);
+    val1 = NULL;
+    val2 = NULL;
+    val3 = NULL;
+}
+
+void test_insert_and_update_function(void) {
+    symtable* table = symtable_create_table(FUNCTION_TABLE);
+    char* key1 = "test_key1";
+    char* key2 = "test_key2";
+    char* key3 = "test_key3";
+
+    char* val1 = copy_string("val1");
+    char* val2 = copy_string("val2");
+    char* val3 = copy_string("val3");
+
+    char *arr1[256] = {val1};
+    char *arr2[256] = {val1, val2};
+    char *arr3[256] = {val1, val2, val3};
+
+
+    symtable_insert_function(table, key1, NONE_TYPE, 1, arr1);
+    symtable_insert_function(table, key2, INTEGER, 2, arr2);
+    symtable_insert_function(table, key3, STRING, 3, arr3);
+
+    funcInfo *info = symtable_get_pair(table, key1);
+    TEST_ASSERT_TRUE(info->numOfParams == 1);
+    TEST_ASSERT_TRUE(info->returnType == NONE_TYPE);
+    TEST_ASSERT_TRUE(compare_strings(info->paramNames[0], "val1") == 1);
+
+    info = symtable_get_pair(table, key2);
+    TEST_ASSERT_TRUE(info->numOfParams == 2);
+    TEST_ASSERT_TRUE(info->returnType == INTEGER);
+
+    info = symtable_get_pair(table, key3);
+    TEST_ASSERT_TRUE(info->numOfParams == 3);
+    TEST_ASSERT_TRUE(info->returnType == STRING);
+    TEST_ASSERT_TRUE(compare_strings(info->paramNames[2], "val3") == 1);
+
+    symtable_delete_pair(table, key1);
+    TEST_ASSERT_NULL(symtable_get_pair(table, key1));
+
+    symtable_insert_function(table, key1, INTEGER, 3, arr3);
+
+    info = symtable_get_pair(table, key1);
+    TEST_ASSERT_TRUE(info->numOfParams == 3);
+    TEST_ASSERT_TRUE(info->returnType == INTEGER);
+    TEST_ASSERT_TRUE(compare_strings(info->paramNames[2], "val3") == 1);
+
+    symtable_delete_table(table);
+    free(val1);
+    free(val2);
+    free(val3);
+    val1 = NULL;
+    val2 = NULL;
+    val3 = NULL;
 }
 
 void test_insert_and_get_variable(void) {
@@ -46,8 +117,8 @@ void test_insert_and_get_variable(void) {
     char* key3 = "test_key3";
 
     symtable_insert_variable(table, key1, true, NONE_TYPE);
-    symtable_insert_function(table, key2, false, INTEGER);
-    symtable_insert_function(table, key3, false, STRING);
+    symtable_insert_variable(table, key2, false, INTEGER);
+    symtable_insert_variable(table, key3, false, STRING);
 
     varInfo *info = symtable_get_pair(table, key1);
     TEST_ASSERT_TRUE(info->isConst);
@@ -78,7 +149,7 @@ void test_insert_and_delete_variable(void) {
     char* key2 = "key_to_delete2";
 
     symtable_insert_variable(table, key1, true, NONE_TYPE);
-    symtable_insert_function(table, key2, false, INTEGER);
+    symtable_insert_variable(table, key2, false, INTEGER);
 
     varInfo *info = symtable_get_pair(table, key1);
     TEST_ASSERT_TRUE(info->isConst);
@@ -94,6 +165,85 @@ void test_insert_and_delete_variable(void) {
     TEST_ASSERT_NOT_NULL(symtable_get_pair(table, key2));
 
     symtable_delete_table(table);
+}
+
+void test_many(void){
+    symtable* table = symtable_create_table(FUNCTION_TABLE);
+    char* key1 = "test_key1";
+    char* key2 = "test_key2";
+    char* key3 = "test_key3";
+
+    char* val1 = copy_string("val1");
+    char* val2 = copy_string("val2");
+    char* val3 = copy_string("val3");
+
+    char *arr1[256] = {val1};
+    char *arr2[256] = {val1, val2};
+    char *arr3[256] = {val1, val2, val3};
+
+
+    symtable_insert_function(table, key1, NONE_TYPE, 1, arr1);
+    symtable_insert_function(table, key2, INTEGER, 2, arr2);
+    symtable_insert_function(table, key3, STRING, 3, arr3);
+
+    funcInfo *info = symtable_get_pair(table, key1);
+    TEST_ASSERT_TRUE(info->numOfParams == 1);
+    TEST_ASSERT_TRUE(info->returnType == NONE_TYPE);
+    TEST_ASSERT_TRUE(compare_strings(info->paramNames[0], "val1") == 1);
+
+    info = symtable_get_pair(table, key2);
+    TEST_ASSERT_TRUE(info->numOfParams == 2);
+    TEST_ASSERT_TRUE(info->returnType == INTEGER);
+
+    info = symtable_get_pair(table, key3);
+    TEST_ASSERT_TRUE(info->numOfParams == 3);
+    TEST_ASSERT_TRUE(info->returnType == STRING);
+    TEST_ASSERT_TRUE(compare_strings(info->paramNames[2], "val3") == 1);
+
+    for (int i = 0; i < 1000; ++i) {
+        char key[20];
+        sprintf(key, "key%d", i);
+        symtable_insert_function(table, key, INTEGER, 2, arr2);
+        info = symtable_get_pair(table, key);
+        TEST_ASSERT_TRUE(info->returnType == INTEGER);
+    }
+
+    for (int i = 0; i < 1000; ++i) {
+        char key[20];
+        sprintf(key, "key%d", i);
+        symtable_insert_function(table, key, STRING, 3, arr3);
+        info = symtable_get_pair(table, key);
+        TEST_ASSERT_TRUE(info->returnType == STRING);
+    }
+
+    for (int i = 0; i < 1000; ++i) {
+        char key[20];
+        sprintf(key, "key%d", i);
+        symtable_delete_pair(table, key);
+        info = symtable_get_pair(table, key);
+        TEST_ASSERT_NULL(info);
+    }
+
+    info = symtable_get_pair(table, key1);
+    TEST_ASSERT_TRUE(info->numOfParams == 1);
+    TEST_ASSERT_TRUE(info->returnType == NONE_TYPE);
+    TEST_ASSERT_TRUE(compare_strings(info->paramNames[0], "val1") == 1);
+
+    info = symtable_get_pair(table, key2);
+    TEST_ASSERT_TRUE(info->numOfParams == 2);
+    TEST_ASSERT_TRUE(info->returnType == INTEGER);
+
+    info = symtable_get_pair(table, key3);
+    TEST_ASSERT_TRUE(info->numOfParams == 3);
+    TEST_ASSERT_TRUE(info->returnType == STRING);
+
+    symtable_delete_table(table);
+    free(val1);
+    free(val2);
+    free(val3);
+    val1 = NULL;
+    val2 = NULL;
+    val3 = NULL;
 }
 //
 //
@@ -136,5 +286,7 @@ int main(void) {
     RUN_TEST(test_insert_and_get_function);
     RUN_TEST(test_insert_and_get_variable);
     RUN_TEST(test_insert_and_delete_variable);
+    RUN_TEST(test_insert_and_update_function);
+    RUN_TEST(test_many);
     return UNITY_END();
 }
